5ad40b8c6b999a86d8bf21efc02dec3d5064cc04
faizi2500 <faizan.zahid2500@gmail.com>
2022-07-12 15:49:00 +0100

    Initial setup for the amazone clone

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+
+const Header = () => (
+  <div>
+    <SearchIcon />
+  </div>
+);
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBjJPGc-YtFgwdqhN14jw0e2ntxeqejwGE',
+  authDomain: 'clone-84db7.firebaseapp.com',
+  projectId: 'clone-84db7',
+  storageBucket: 'clone-84db7.appspot.com',
+  messagingSenderId: '322979396805',
+  appId: '1:322979396805:web:f0a040d30f219183c704ac',
+  measurementId: 'G-K7WWDNF6XY',
+};
