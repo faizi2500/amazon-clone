1af316558dcdbf1440f7ca589fc2f6c790f869fa
faizi2500 <faizan.zahid2500@gmail.com>
2022-07-21 22:32:49 +0100

    User authentication set up

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -2,17 +2,25 @@ const AppReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
       return {
+        ...state,
         shoppingList: [...state.shoppingList, action.payload],
       };
     case 'REMOVE_ITEM':
       return {
+        ...state,
         shoppingList: state.shoppingList.filter(
           (item, index) => index !== action.location,
         ),
       };
     case 'CLEAR ALL':
       return {
+        ...state,
         shoppingList: [],
+      };
+    case 'User Authorized':
+      return {
+        ...state,
+        user: action.payload,
       };
     default:
       return state;
